6d8836a0a088a70d85c997377ae475063c0c2d88
Nick Freeman <anisiniko@gmail.com>
2018-05-17 18:00:28 +0000

    Add operations for filter and cache klines

---
+++ app/lib/app/parser/operations/cache_all_kline1m.rb
@@ -0,0 +1,34 @@
+require 'app/operation'
+require 'parser/import'
+
+module App
+  module Parser
+    module Operations
+      # Save all kline to redis
+      class CacheAllKline1m < App::Operation
+        include App::Import[
+          kline1m: 'persistence.repositories.kline1m',
+          redis: 'redis.binance'
+        ]
+        def call(params)
+          puts params
+          print "\nLoad all kline1m from DB. Wait for a while..."
+          all_klines = {1 => kline1m.last.to_h}
+          print "\nDone. Loaded #{all_klines.size} records from Postgres"
+
+          print "\nCache all klines in redis..."
+          prefix = params[:c1] + params[:c2] + ':' + 'kline' + params[:interval] + ':'
+          memo = 0
+          all_klines.each do |id, kline|
+            memo += 1
+            k_name = prefix + kline[:open_time].to_s
+            redis.set(k_name, kline.to_json)
+          end
+          print "\nDone. Cached #{memo} klines\n"
+
+          Success(params)
+        end
+      end
+    end
+  end
+end
